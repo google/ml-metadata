93fd991304da907597ba2becf42e1ef7db121c29
ml-metadata-team <no-reply@google.com>
2020-04-03 15:28:46 -0700

    Refactor mysql_metadata_source_tests. Refactored tests will come from instantiated test suite in metadata_source_test_suite.cc.
    PiperOrigin-RevId: 304705127

--- ml_metadata/metadata_store/mysql_metadata_source_test.cc
+++ ml_metadata/metadata_store/mysql_metadata_source_test.cc
@@ -1,4 +1,4 @@
-/* Copyright 2019 Google LLC
+/* Copyright 2020 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,20 +17,12 @@ limitations under the License.
 #include <memory>
 
 #include "gflags/gflags.h"
-#include "google/protobuf/text_format.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/memory/memory.h"
-#include "absl/strings/str_cat.h"
-#include "absl/strings/substitute.h"
-#include "ml_metadata/metadata_store/constants.h"
 #include "ml_metadata/metadata_store/metadata_source.h"
+#include "ml_metadata/metadata_store/metadata_source_test_suite.h"
 #include "ml_metadata/metadata_store/test_mysql_metadata_source_initializer.h"
-#include "ml_metadata/metadata_store/test_util.h"
-#include "ml_metadata/proto/metadata_source.pb.h"
-#include "ml_metadata/proto/metadata_store.pb.h"
-#include "tensorflow/core/lib/core/status.h"
-#include "tensorflow/core/lib/core/status_test_util.h"
 
 DEFINE_bool(enable_sockets, true, "Whether to run socket tests.");
 
@@ -38,21 +30,21 @@ namespace ml_metadata {
 namespace testing {
 namespace {
 
-using ::tensorflow::Status;
-
-class MySqlMetadataSourceTest : public ::testing::Test {
- protected:
-  void SetUp() override {
+class MysqlMetadataSourceContainer : public MetadataSourceContainer {
+ public:
+  MysqlMetadataSourceContainer() : MetadataSourceContainer() {
     metadata_source_initializer_ = GetTestMySqlMetadataSourceInitializer();
-    // Use TCP by default. Tests that need other connection types should not use
-    // this test fixture.
+    // Use TCP connection type for common Mysql metadata source tests.
     metadata_source_ = metadata_source_initializer_->Init(
         TestMySqlMetadataSourceInitializer::ConnectionType::kTcp);
   }
 
-  void TearDown() override { metadata_source_initializer_->Cleanup(); }
+  ~MysqlMetadataSourceContainer() override = default;
 
-  void InitTestSchema() {
+  MetadataSource* GetMetadataSource() override { return metadata_source_; }
+
+  // InitTestSchema creates a new table t1(c1 INT, c2 VARCHAR(255)).
+  void InitTestSchema() override {
     TF_CHECK_OK(metadata_source_->Connect());
     TF_CHECK_OK(metadata_source_->Begin());
     TF_CHECK_OK(metadata_source_->ExecuteQuery(
@@ -60,7 +52,9 @@ class MySqlMetadataSourceTest : public ::testing::Test {
     TF_CHECK_OK(metadata_source_->Commit());
   }
 
-  void InitSchemaAndPopulateRows() {
+  // InitSchemaAndPopulateRows creates table t1(c1 INT, c2 VARCHAR(255)) and
+  // adds 3 rows to this table: (1,'v1'), (2,'v2'), (3, 'v3').
+  void InitSchemaAndPopulateRows() override {
     InitTestSchema();
     TF_CHECK_OK(metadata_source_->Begin());
     TF_CHECK_OK(metadata_source_->ExecuteQuery(
@@ -72,6 +66,7 @@ class MySqlMetadataSourceTest : public ::testing::Test {
     TF_CHECK_OK(metadata_source_->Commit());
   }
 
+ private:
   // An unowned TestMySqlMetadataSourceInitializer from a call to
   // GetTestMySqlMetadataSourceInitializer().
   std::unique_ptr<TestMySqlMetadataSourceInitializer>
@@ -82,9 +77,9 @@ class MySqlMetadataSourceTest : public ::testing::Test {
 };
 
 // This test is to verify we can connect to the DB using sockets.
-// We use a fixtureless test to avoid conflicting with the default
-// metadata_source_initializer defined in MySqlMetadataSourceTest.
-TEST(MySqlMetadataSourceSocketTest, TestConnectBySocket) {
+// We use a fixtureless test because it does not share the same connection type
+// with TestEscapeString below.
+TEST(MySqlMetadataSourceExtendedTest, TestConnectBySocket) {
   // TODO(b/140584643) Fix MacOS Kokoro test to enable connecting via sockets.
   if (!FLAGS_enable_sockets) {
     GTEST_SKIP() << "Socket tests disabled.";
@@ -101,166 +96,27 @@ TEST(MySqlMetadataSourceSocketTest, TestConnectBySocket) {
   metadata_source_initializer->Cleanup();
 }
 
-TEST_F(MySqlMetadataSourceTest, TestQueryWithoutConnect) {
-  Status s =
-      metadata_source_->ExecuteQuery("CREATE TABLE foo(bar INT)", nullptr);
-  EXPECT_EQ(s.code(), tensorflow::error::FAILED_PRECONDITION);
-}
-
-TEST_F(MySqlMetadataSourceTest, TestInsert) {
-  InitTestSchema();
-  TF_ASSERT_OK(metadata_source_->Begin());
-  TF_ASSERT_OK(metadata_source_->ExecuteQuery("INSERT INTO t1 VALUES (1, 'v1')",
-                                              nullptr));
-  RecordSet expected_results;
-  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
-      R"(column_names: "c1"
-         column_names: "c2"
-         records: {
-            values: "1"
-            values: "v1"
-         })",
-      &expected_results));
-
-  RecordSet query_results;
-  TF_ASSERT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  TF_ASSERT_OK(metadata_source_->Commit());
-
-  EXPECT_EQ(1, query_results.records().size());
-  EXPECT_THAT(query_results, EqualsProto(expected_results));
-}
-
-// TODO(b/149022372): add this test to the shared metadata_source test suite.
-TEST_F(MySqlMetadataSourceTest, TestNull) {
-  InitTestSchema();
-  TF_EXPECT_OK(metadata_source_->Begin());
-  TF_EXPECT_OK(metadata_source_->ExecuteQuery("INSERT INTO t1 VALUES (1, NULL)",
-                                              nullptr));
-  RecordSet expected_results = ParseTextProtoOrDie<RecordSet>(
-      absl::Substitute(
-        R"(column_names: "c1"
-           column_names: "c2"
-           records: { values: "1" values: "$0" })", kMetadataSourceNull));
-
-  RecordSet query_results;
-  TF_EXPECT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  TF_EXPECT_OK(metadata_source_->Commit());
-  EXPECT_EQ(1, query_results.records().size());
-  EXPECT_THAT(query_results, EqualsProto(expected_results));
-}
-
-
-TEST_F(MySqlMetadataSourceTest, TestEscapeString) {
-  TF_CHECK_OK(metadata_source_->Connect());
-  EXPECT_EQ(metadata_source_->EscapeString("''"), "\\'\\'");
-  EXPECT_EQ(metadata_source_->EscapeString("'\'"), "\\'\\\'");
-  EXPECT_EQ(metadata_source_->EscapeString("'\"text\"'"), "\\'\\\"text\\\"\\'");
-}
-
-TEST_F(MySqlMetadataSourceTest, TestInsertWithEscapedStringValue) {
-  InitTestSchema();
-  TF_ASSERT_OK(metadata_source_->Begin());
-  TF_ASSERT_OK(metadata_source_->ExecuteQuery(
-      absl::StrCat("INSERT INTO t1 VALUES (1, '",
-                   metadata_source_->EscapeString("''"), "')"),
-      nullptr));
-  RecordSet expected_results;
-  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
-      R"(column_names: "c1"
-         column_names: "c2"
-         records: {
-            values: "1"
-            values: "''"
-         })",
-      &expected_results));
-
-  RecordSet query_results;
-  TF_ASSERT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  TF_EXPECT_OK(metadata_source_->Commit());
-  EXPECT_EQ(1, query_results.records().size());
-  EXPECT_THAT(query_results, EqualsProto(expected_results));
-}
-
-TEST_F(MySqlMetadataSourceTest, TestDelete) {
-  InitSchemaAndPopulateRows();
-  RecordSet query_results;
-  TF_ASSERT_OK(metadata_source_->Begin());
-  TF_ASSERT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  ASSERT_LT(0, query_results.records().size());
-
-  TF_ASSERT_OK(metadata_source_->ExecuteQuery("DELETE FROM t1", nullptr));
-
-  query_results.Clear();
-  TF_ASSERT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  TF_ASSERT_OK(metadata_source_->Commit());
-  EXPECT_EQ(0, query_results.records().size());
-}
-
-TEST_F(MySqlMetadataSourceTest, TestUpdate) {
-  InitSchemaAndPopulateRows();
-  TF_ASSERT_OK(metadata_source_->Begin());
-  TF_ASSERT_OK(metadata_source_->ExecuteQuery(
-      "UPDATE t1 SET c2 = 'v100' WHERE c1 = 1", nullptr));
-
-  RecordSet expected_results;
-  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
-      R"(column_names: "c1"
-         column_names: "c2"
-         records: {
-            values: "1"
-            values: "v100"
-         })",
-      &expected_results));
-
-  RecordSet query_results;
-  TF_EXPECT_OK(metadata_source_->ExecuteQuery("SELECT * FROM t1 WHERE c1 = 1",
-                                              &query_results));
-  EXPECT_THAT(query_results, EqualsProto(expected_results));
-  TF_ASSERT_OK(metadata_source_->Commit());
-}
-
-// multiple queries, and rollback, nothing should happen
-TEST_F(MySqlMetadataSourceTest, TestMultiQueryTransaction) {
-  InitSchemaAndPopulateRows();
-  RecordSet expected_results;
-  TF_EXPECT_OK(metadata_source_->Begin());
-  TF_EXPECT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &expected_results));
-  TF_EXPECT_OK(metadata_source_->Commit());
-
-  // disable auto commit
-  TF_EXPECT_OK(metadata_source_->Begin());
-  TF_EXPECT_OK(metadata_source_->ExecuteQuery("DELETE FROM t1", nullptr));
-  TF_EXPECT_OK(metadata_source_->Rollback());
-
-  // rollback the delete query, there should be no change in the database
-  RecordSet query_results;
-  TF_EXPECT_OK(metadata_source_->Begin());
-  TF_EXPECT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  EXPECT_THAT(query_results, EqualsProto(expected_results));
-
-  // now it is in a new transaction, insert two records, then commit
-  TF_EXPECT_OK(metadata_source_->ExecuteQuery("INSERT INTO t1 VALUES (1, 'v1')",
-                                              nullptr));
-  TF_EXPECT_OK(metadata_source_->ExecuteQuery("INSERT INTO t1 VALUES (2, 'v2')",
-                                              nullptr));
-  TF_EXPECT_OK(metadata_source_->Commit());
-  query_results.Clear();
-  TF_EXPECT_OK(metadata_source_->Begin());
-  TF_EXPECT_OK(
-      metadata_source_->ExecuteQuery("SELECT * FROM t1", &query_results));
-  int expected_num_rows = expected_results.records().size() + 2;
-  TF_EXPECT_OK(metadata_source_->Commit());
-  EXPECT_EQ(expected_num_rows, query_results.records().size());
+// Test EscapeString utility method.
+// Same here, we adopt a fixtureless test here because it is using TCP
+// connection type, different from TestConnectBySocket.
+TEST(MySqlMetadataSourceExtendedTest, TestEscapeString) {
+  auto metadata_source_initializer = GetTestMySqlMetadataSourceInitializer();
+  auto metadata_source = metadata_source_initializer->Init(
+      TestMySqlMetadataSourceInitializer::ConnectionType::kTcp);
+  TF_CHECK_OK(metadata_source->Connect());
+  EXPECT_EQ(metadata_source->EscapeString("''"), "\\'\\'");
+  EXPECT_EQ(metadata_source->EscapeString("'\'"), "\\'\\\'");
+  EXPECT_EQ(metadata_source->EscapeString("'\"text\"'"), "\\'\\\"text\\\"\\'");
 }
 
 }  // namespace
+
+INSTANTIATE_TEST_CASE_P(
+    MysqlMetadataSourceCommonTest, MetadataSourceTestSuite,
+    ::testing::Values([]() {
+      return absl::make_unique<MysqlMetadataSourceContainer>();
+    }));
+
 }  // namespace testing
 }  // namespace ml_metadata
 
