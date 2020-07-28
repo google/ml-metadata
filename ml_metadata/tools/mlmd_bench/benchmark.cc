b5395bd920c1f7d7dbca0e2fcc13cc705b534b54
huimiao <huimiao@google.com>
2020-07-28 11:24:28 -0700

    Adds ThreadRunner to run Benchmark with Workloads end-to-end.
    PiperOrigin-RevId: 323614684

---
+++ ml_metadata/tools/mlmd_bench/benchmark.cc
@@ -0,0 +1,54 @@
+/* Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "ml_metadata/tools/mlmd_bench/benchmark.h"
+
+#include <vector>
+
+#include "absl/memory/memory.h"
+#include "ml_metadata/metadata_store/types.h"
+#include "ml_metadata/tools/mlmd_bench/fill_types_workload.h"
+#include "ml_metadata/tools/mlmd_bench/proto/mlmd_bench.pb.h"
+#include "ml_metadata/tools/mlmd_bench/workload.h"
+
+namespace ml_metadata {
+namespace {
+
+// Creates the executable workload given `workload_config`.
+void CreateWorkload(const WorkloadConfig& workload_config,
+                    std::unique_ptr<WorkloadBase>& workload) {
+  if (!workload_config.has_fill_types_config()) {
+    LOG(FATAL) << "Cannot find corresponding workload!";
+  }
+  workload = absl::make_unique<FillTypes>(FillTypes(
+      workload_config.fill_types_config(), workload_config.num_operations()));
+}
+
+}  // namespace
+
+Benchmark::Benchmark(const MLMDBenchConfig& mlmd_bench_config) {
+  workloads_.resize(mlmd_bench_config.workload_configs_size());
+
+  // For each `workload_config`, calls CreateWorkload() to create corresponding
+  // workload.
+  for (int i = 0; i < mlmd_bench_config.workload_configs_size(); ++i) {
+    CreateWorkload(mlmd_bench_config.workload_configs(i), workloads_[i]);
+  }
+}
+
+WorkloadBase* Benchmark::workload(const int64 workload_index) {
+  return workloads_[workload_index].get();
+}
+
+}  // namespace ml_metadata
---
+++ ml_metadata/tools/mlmd_bench/thread_runner.h
@@ -0,0 +1,45 @@
+/* Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef ML_METADATA_TOOLS_MLMD_BENCH_THREAD_RUNNER_H
+#define ML_METADATA_TOOLS_MLMD_BENCH_THREAD_RUNNER_H
+
+#include "ml_metadata/proto/metadata_store.pb.h"
+#include "ml_metadata/tools/mlmd_bench/benchmark.h"
+#include "ml_metadata/tools/mlmd_bench/proto/mlmd_bench.pb.h"
+#include "tensorflow/core/lib/core/status.h"
+
+namespace ml_metadata {
+
+// The ThreadRunner class is the execution component of the `mlmd_bench`
+// It takes the benchmark and runs the workloads.
+class ThreadRunner {
+ public:
+  ThreadRunner(const ConnectionConfig& mlmd_config, int64 num_threads);
+  ~ThreadRunner() = default;
+
+  // Execution unit of `mlmd_bench`.
+  // Returns detailed error if query executions failed.
+  tensorflow::Status Run(Benchmark& benchmark);
+
+ private:
+  // Connection configuration that will be used to create the MetadataStore.
+  const ConnectionConfig mlmd_config_;
+  // Number of threads for the thread runner.
+  const int64 num_threads_;
+};
+
+}  // namespace ml_metadata
+
+#endif  // ML_METADATA_TOOLS_MLMD_BENCH_THREAD_RUNNER_H
