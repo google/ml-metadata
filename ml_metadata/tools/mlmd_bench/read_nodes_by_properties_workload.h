f798265f59c7d9f2729048c7140426c7ed79f674
BrianSong <briansong0122@gmail.com>
2020-08-16 12:03:01 -0400

    Update read_nodes_by_properties_workload.h

--- ml_metadata/tools/mlmd_bench/read_nodes_by_properties_workload.h
+++ ml_metadata/tools/mlmd_bench/read_nodes_by_properties_workload.h
@@ -57,7 +57,7 @@ class ReadNodesByProperties
   // Number of operations for the current workload.
   const int64 num_operations_;
   // String for indicating the name of current workload instance.
-  std::string name_;
+  const std::string name_;
 };
 
 }  // namespace ml_metadata
